6c1c66d61d4457a04b02e82dfce963d87ad53672
Arshia Majidi <arshiamajidi@ymail.com>
2019-06-14 02:45:07 +0430

    register fix

--- app/src/main/java/org/arshef/finalproject/Tools/StaticTools.java
+++ app/src/main/java/org/arshef/finalproject/Tools/StaticTools.java
@@ -1,4 +1,26 @@
 package org.arshef.finalproject.Tools;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.Toast;
+
+import org.arshef.finalproject.Activities.LoginActivity;
+import org.arshef.finalproject.Models.User;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StaticTools {
+    public static void ToastMaker(Context context, String text) {
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+    }
+
+    public static boolean Authenticate(String username) {
+        if (User.find(User.class, "Username = ?", username).size() > 0) {
+            return true;
+        }
+        return false;
+    }
 }
