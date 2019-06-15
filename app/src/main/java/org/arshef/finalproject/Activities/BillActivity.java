bd0311ff963fd5f18e456d7dec21da8266473a01
storm <storm.aref@gmail.com>
2019-06-15 14:14:59 +0430

    bill

--- app/src/main/java/org/arshef/finalproject/Activities/BillActivity.java
+++ app/src/main/java/org/arshef/finalproject/Activities/BillActivity.java
@@ -1,6 +1,15 @@
 package org.arshef.finalproject.Activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import org.arshef.finalproject.R;
+
 public class BillActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill);
+    }
 }
