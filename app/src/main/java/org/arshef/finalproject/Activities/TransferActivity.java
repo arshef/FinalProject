ac108806d5fe4f524f90033a8589402c6acd02df
Arshia Majidi <arshiamajidi@ymail.com>
2019-06-15 13:04:45 +0430

    db initializer

--- app/src/main/java/org/arshef/finalproject/Activities/TransferActivity.java
+++ app/src/main/java/org/arshef/finalproject/Activities/TransferActivity.java
@@ -1,4 +1,25 @@
 package org.arshef.finalproject.Activities;
 
-public class TransferActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import org.arshef.finalproject.R;
+
+public class TransferActivity extends AppCompatActivity {
+    EditText originNo, accPass, destinationNo, amount;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer);
+        originNo = findViewById(R.id.origin_cardnum);
+        accPass = findViewById(R.id.accountpass);
+        destinationNo = findViewById(R.id.destinationacc);
+        amount = findViewById(R.id.transferamount);
+
+
+
+    }
 }
